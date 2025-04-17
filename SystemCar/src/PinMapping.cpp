55654591a0aeddd64754c345fdd1b704db2b4382
Clement Okie <83030323+ClementM1998@users.noreply.github.com>
2025-04-17 07:57:08 -0400

    Update PinMapping.cpp

--- SystemCar/src/PinMapping.cpp
+++ SystemCar/src/PinMapping.cpp
@@ -16,5 +16,40 @@ void PinMapping::init() {
     /* DOORLOCKPIN positif dan negatif */
     pinMode(DOORLOCKPIN_P, OUTPUT);
     pinMode(DOORLOCKPIN_N, OUTPUT);
-    
+    /* WINDOWMAIN positif dan negatif */
+    pinMode(WINDOWMAIN_P, OUTPUT);
+    pinMode(WINDOWMAIN_N, OUTPUT);
+    /* WINDOWLEFT positif dan negatif */
+    pinMode(WINDOWLEFT_P, OUTPUT);
+    pinMode(WINDOWLEFT_N, OUTPUT);
+    /* WINDOWREARRIGHT positif dan negatif */
+    pinMode(WINDOWREARRIGHT_P, OUTPUT);
+    pinMode(WINDOWREARRIGHT_N, OUTPUT);
+    /* WINDOWREARLEFT positif dan negatif */
+    pinMode(WINDOWREARLEFT_P, OUTPUT);
+    pinMode(WINDOWREARLEFT_N, OUTPUT);
+    /* HIGHLIGHTLEFT dan HIGHLIGHTRIGHT */
+    pinMode(HIGHLIGHTLEFT, OUTPUT);
+    pinMode(HIGHLIGHTRIGHT, OUTPUT);
+    /* LOWLIGHTLEFT dan LOWLIGHTRIGHT */
+    pinMode(LOWLIGHTLEFT, OUTPUT);
+    pinMode(LOWLIGHTRIGHT, OUTPUT);
+    /* FOGLIGHTLEFT dan FOGLIGHTRIGHT */
+    pinMode(FOGLIGHTLEFT, OUTPUT);
+    pinMode(FOGLIGHTRIGHT, OUTPUT);
+    /* RUNNINGLIGHTLEFT dan RUNNINGLIGHTRIGHT */
+    pinMode(RUNNINGLIGHTLEFT, OUTPUT);
+    pinMode(RUNNINGLIGHTRIGHT, OUTPUT);
+}
+
+void PinMapping::turnOn(int pin) {
+    digitalWrite(pin, HIGH);
+}
+
+void PinMapping::turnOff(int pin) {
+    digitalWrite(pin, LOW);
+}
+
+void PinMapping::pulse(int pin, int value) {
+    analogWrite(pin, value);
 }
